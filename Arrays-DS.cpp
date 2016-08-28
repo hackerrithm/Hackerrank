eed4b42628cd45e405a5cee3798c969310efab6b
Kemar Galloway <kemargalloway@gmail.com>
2016-08-28 15:04:20 -0500

    Arrays Solution

---
+++ Arrays-DS.cpp
@@ -0,0 +1,41 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for(int arr_i = 0;arr_i < n;arr_i++)
+    {
+       cin >> arr[arr_i];
+    }
+    n = n-1;
+    for(int i = n; i >= 0; i--)
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
